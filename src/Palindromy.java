24b4209779a87a72cd8983db96c5193cf3176e70
Marcin <mciurak@wp.pl>
2019-11-22 21:44:51 +0100

    Drugi commit.

--- src/Palindromy.java
+++ src/Palindromy.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class Palindromy {
+    public static void main(String[] args) {
+        String tekst = "albo nałowi ci Wałęsa masę ławic i woła Nobla";
+
+        boolean jestPalindomem = true;
+        char[] znaki = tekst.trim().toUpperCase().replace(" ", "").toCharArray();
+        for (int i = 0; i <= znaki.length/2; i++) {
+            // znak z drugiego konca tablicy
+            char znakOdpowiadajacy = znaki[znaki.length - i - 1];
+            System.out.println("Znak to: " + znaki[i] + " a odpowiada mu " + znakOdpowiadajacy);
+            if (znaki[i] != znakOdpowiadajacy) {
+                jestPalindomem = false;
+                break;
+            }
+        }
+
+        if (jestPalindomem) {
+            System.out.println("Sukces! Mamy palindom!");
+        } else {
+            System.out.println("To nie jest palindrom!");
+        }
+    }
 }
+
+
